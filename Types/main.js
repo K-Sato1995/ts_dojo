ed06d838328d5d3dcefa238e1fbc95f31be704a6
Katsuki <Katsuki_Sato0130@icloud.com>
2019-07-29 19:32:24 +0900

    Add Types

---
+++ Types/main.js
@@ -0,0 +1,8 @@
+// Basic Types
+var nickName = "K-Sato";
+var age = 123;
+var male = true;
+console.log(nickName);
+// Arrays
+var strs;
+strs = ["A", "B"];
---
+++ Types/main.ts
@@ -0,0 +1,10 @@
+// Basic Types
+const nickName: string = "K-Sato";
+var age: number = 123;
+var male: boolean = true;
+
+console.log(nickName);
+
+// Arrays
+var strs: string[];
+strs = ["A", "B"];
