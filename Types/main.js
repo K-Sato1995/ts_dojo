a3a06a71081d528b2af1a9c15b47c3a4638c4703
Katsuki <Katsuki_Sato0130@icloud.com>
2019-07-29 19:38:22 +0900

    Add enum

--- Types/main.js
+++ Types/main.js
@@ -2,7 +2,19 @@
 var nickName = "K-Sato";
 var age = 123;
 var male = true;
-console.log(nickName);
+var nationality = "";
+console.log(nickName, age, male, nationality); //=> K-Sato 123 true
 // Arrays
-var strs;
-strs = ["A", "B"];
+var strs = ["A", "B"];
+var nums = [1, 2, 3];
+var nums2 = [1, 2, 3];
+var person = ["K-Sato", 234];
+console.log(strs, nums, nums2, person); ///=> [ 'A', 'B' ] [ 1, 2, 3 ] [ 1, 2, 3 ]
+// Enums
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+console.log(Color.Red); //=> 1
--- Types/main.ts
+++ Types/main.ts
@@ -1,10 +1,24 @@
 // Basic Types
-const nickName: string = "K-Sato";
-var age: number = 123;
-var male: boolean = true;
+var nickName: string = "K-Sato";
+const age: number = 123;
+let male: boolean = true;
+let nationality: any = "";
 
-console.log(nickName);
+console.log(nickName, age, male, nationality); //=> K-Sato 123 true
 
 // Arrays
-var strs: string[];
-strs = ["A", "B"];
+var strs: string[] = ["A", "B"];
+let nums: number[] = [1, 2, 3];
+let nums2: Array<number> = [1, 2, 3];
+let person: [string, number] = ["K-Sato", 234];
+
+console.log(strs, nums, nums2, person); ///=> [ 'A', 'B' ] [ 1, 2, 3 ] [ 1, 2, 3 ]
+
+// Enums
+enum Color {
+  Red,
+  Green,
+  Blue
+}
+
+console.log(Color.Red); //=> 0
