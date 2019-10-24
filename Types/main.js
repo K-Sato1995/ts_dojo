397b243c930827747e2de6a3db3458487a1adc6b
Katsuki <Katsuki_Sato0130@icloud.com>
2019-10-24 10:51:50 +0900

    Update Basic types

--- Types/main.js
+++ Types/main.js
@@ -1,14 +1,14 @@
 // Basic Types
 var nickName = "K-Sato";
-var age = 123;
+var age = 24;
 var male = true;
-var nationality = "";
-console.log(nickName, age, male, nationality); //=> K-Sato 123 true
+var extraInfo = { natinality: "Japan", pet: "Dog" };
+console.log(nickName, age, male, extraInfo); //=> K-Sato 123 true { natinality: 'Japan', pet: 'Dog' }
 // Arrays
-var strs = ["A", "B"];
-var nums = [1, 2, 3];
-var nums2 = [1, 2, 3];
-console.log(strs, nums, nums2); ///=> [ 'A', 'B' ] [ 1, 2, 3 ] [ 1, 2, 3 ]
+var strArray = ["A", "B"];
+var numArray = [1, 2, 3];
+var numArray2 = [1, 2, 3];
+console.log(strArray, numArray, numArray2); ///=> [ 'A', 'B' ] [ 1, 2, 3 ] [ 1, 2, 3 ]
 // Enums
 var Color;
 (function (Color) {
@@ -18,13 +18,17 @@ var Color;
 })(Color || (Color = {}));
 console.log(Color.Red); //=> 0
 // Union Type
+var year;
+year = 24;
+year = "24";
+console.log(typeof year);
 function me(info) {
     console.log(info);
 }
-console.log(me("K-Sato"));
-console.log(me(562));
+console.log(me("K-Sato")); //=> K-Sato
+console.log(me(24)); //=> 24
 // Tuple Type
 var person = ["K-Sato", 234];
-console.log(person);
+console.log(person); //=> [ 'K-Sato', 234 ]
 var log = 2;
-console.log(log); //=>
+console.log(log); //=> 2
