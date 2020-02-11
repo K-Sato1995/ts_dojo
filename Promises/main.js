8f2b54c1a4acc7341db1695cd00bc5dfdcde784a
Katsuki <Katsuki_Sato0130@icloud.com>
2020-02-11 20:30:58 +0900

    Add Rejected Promise

--- Promises/main.js
+++ Promises/main.js
@@ -1,6 +1,12 @@
-var one = new Promise(function (resolve, reject) {
+var promiseResolved = new Promise(function (resolve, reject) {
     resolve("Hello");
 });
-one.then(function (value) {
-    console.log("resolved", value);
+promiseResolved.then(function (value) {
+    console.log("resolved", value); //=> resolved Hello
+});
+var promiseRejected = new Promise(function (resolve, reject) {
+    reject(new Error("Whoops!"));
+});
+promiseRejected["catch"](function (error) {
+    console.log("rejected", error);
 });
--- Promises/main.ts
+++ Promises/main.ts
@@ -1,7 +1,15 @@
-const one = new Promise<string>((resolve, reject) => {
+const promiseResolved = new Promise<string>((resolve, reject) => {
   resolve("Hello");
 });
 
-one.then(value => {
+promiseResolved.then(value => {
   console.log("resolved", value); //=> resolved Hello
+});
+
+const promiseRejected = new Promise<string>((resolve, reject) => {
+  reject(new Error("Whoops!"));
+});
+
+promiseRejected.catch(error => {
+  console.log("rejected", error); //=> rejected Error: Whoops!
 });
