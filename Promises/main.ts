c2a077c9330df9a8a7dbf2290d800126f244ab80
Katsuki <Katsuki_Sato0130@icloud.com>
2020-02-14 07:45:06 +0900

    Add prommise examples

--- Promises/main.ts
+++ Promises/main.ts
@@ -1,18 +1,42 @@
 // resolve(value) — if the job finished successfully, with result value.
 // reject(error) — if an error occurred, error is the error object.
 
-const promiseResolved = new Promise<string>((resolve, reject) => {
+// Resolve And Reject
+const promiseResolved = new Promise<string>((resolve, reject):
+  | string
+  | void => {
   resolve("Hello");
+  reject("Error Occured");
 });
 
 promiseResolved.then(value => {
   console.log("resolved", value); //=> resolved Hello
 });
 
-const promiseRejected = new Promise<string>((resolve, reject) => {
+const promiseRejected = new Promise<string>((resolve, reject):
+  | string
+  | void => {
   reject(new Error("Whoops!"));
+  resolve("Nothing ever happed");
 });
 
 promiseRejected.catch(error => {
   console.log("rejected", error); //=> rejected Error: Whoops!
+});
+
+// Chaining Promises
+const promise1 = new Promise<number>((resolve, reject): number | void => {
+  resolve(5);
+});
+
+const promise2 = (num: number): Promise<number> => {
+  return new Promise<number>((resolve, reject): number | void => {
+    resolve(num + 5);
+  });
+};
+
+promise1.then(result => {
+  promise2(result).then(result => {
+    console.log(result); //=> 10
+  });
 });
